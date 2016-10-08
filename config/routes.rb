6f62329dfe3fd24e86bfeb112e579d536d8344ef
Nguyen Thien Duc <1312149@student.hcmus.edu.vn>
2016-10-08 16:49:02 +0700

    Finish add follow users

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,18 @@ Rails.application.routes.draw do
  root 'static_pages#home'
  get    '/sessions/new',   to: 'sessions#new'
  post   '/sessions/new',   to: 'sessions#create'
+
  delete '/logout',  to: 'sessions#destroy'
 
-resources :users
+resources :users do
+    member do
+      get :following, :followers
+    end
+end
+
+
 resources :sessions
 resources :statuses,          only: [:create, :destroy]
-
+resources :relationships,       only: [:create, :destroy]
  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006141020) do
+ActiveRecord::Schema.define(version: 20161008063807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at", using: :btree
+    t.index ["user_id"], name: "index_microposts_on_user_id", using: :btree
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+    t.index ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
+  end
 
   create_table "statuses", force: :cascade do |t|
     t.text     "content"
@@ -30,8 +49,11 @@ ActiveRecord::Schema.define(version: 20161006141020) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "microposts", "users"
   add_foreign_key "statuses", "users"
 end
